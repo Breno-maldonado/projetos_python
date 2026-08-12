866d3fb0e613efc69a7a296a96c797d430d3697c
Breno Maldonado <maldonadobreno12@hotmail.com>
2026-08-12 00:02:22 -0300

    wip: Cor e Label

--- calculadora_de_idade/main.py
+++ calculadora_de_idade/main.py
@@ -7,14 +7,17 @@ janela.geometry("400x500")
 janela.resizable(False, False)
 
 # Cores
-cor1 = "#2e2e2e"
-cor2 = "#ffffff"
-cor3 = "#3f6969"
-cor4 = "#D4D4D4"
-cor5 = "#FFAB40"
+cor1 = "#1E1E1E"
+cor2 = "#2f2f2f"
 
 # Frames
 frame_cima = Frame(janela, width=400, height=180, pady=0, padx=0, relief=SOLID, bg=cor1)
 frame_cima.grid(row=0, column=0)
+frame_baixo = Frame(janela, width=400, height=400, pady=0, padx=0, relief=SOLID, bg=cor2)
+frame_baixo.grid(row=1, column=0)
+
+# Label frames de cima
+label_calculadora = Label(frame_cima, text='Calculadora de Idade', width=25, height=1, padx=3, relief=SOLID, anchor=CENTER, bg=cor2, font=("Ivy 13 bold"))
+label_calculadora.place(relx=0.5, y=40, anchor=CENTER)
 
 janela.mainloop()
