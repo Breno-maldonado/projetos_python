5686d6bfea9eee684f8f952efe3b54e881cdb59f
Breno Maldonado <maldonadobreno12@hotmail.com>
2026-08-13 22:07:13 -0300

    wip: novas cores e texto

--- calculadora_de_idade/main.py
+++ calculadora_de_idade/main.py
@@ -9,6 +9,8 @@ janela.resizable(False, False)
 # Cores
 cor1 = "#1E1E1E"
 cor2 = "#2f2f2f"
+cor3 = "#ffffff"
+cor4 = "#ffb300"
 
 # Frames
 frame_cima = Frame(janela, width=400, height=180, pady=0, padx=0, relief=SOLID, bg=cor1)
@@ -17,7 +19,10 @@ frame_baixo = Frame(janela, width=400, height=400, pady=0, padx=0, relief=SOLID,
 frame_baixo.grid(row=1, column=0)
 
 # Label frames de cima
-label_calculadora = Label(frame_cima, text='Calculadora de Idade', width=25, height=1, padx=3, relief=SOLID, anchor=CENTER, bg=cor2, font=("Ivy 13 bold"))
-label_calculadora.place(relx=0.5, y=40, anchor=CENTER)
+label_calculadora = Label(frame_cima, text='CALCULADORA', width=25, height=1, padx=3, anchor=CENTER, bg=cor1, fg=cor3, font=("Ivy 18 bold"))
+label_calculadora.place(relx=0.5, y=60, anchor=CENTER)
+
+label_idade = Label(frame_cima, text='DE IDADE', width=25, height=1, padx=3, anchor=CENTER, bg=cor1, fg=cor4, font=("Arial 38 bold"))
+label_idade.place(relx=0.5, y=110, anchor=CENTER)
 
 janela.mainloop()
