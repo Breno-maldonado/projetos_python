1f39301efaed233cf46db28aa47518b7a180776b
Breno Maldonado <maldonadobreno12@hotmail.com>
2026-08-17 22:22:42 -0300

    init: janela do layout

---
+++ basico/calculadora_unidade_medida/main.py
@@ -0,0 +1,15 @@
+from tkinter import *
+from tkinter import ttk
+
+# Cores
+cor1 = '#000000'
+cor2 = '#ffffff'
+cor3 = '#3b3b3b'
+
+janela = Tk()
+janela.title('')
+
+janela.geometry('800x400')
+janela.configure(bg = cor3)
+
+janela.mainloop()
