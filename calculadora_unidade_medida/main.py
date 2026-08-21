1369b9bd8fa0f296e631a0b79b35aa9e831602d4
Breno Maldonado <maldonadobreno12@hotmail.com>
2026-08-20 22:31:03 -0300

    add: tema da janela

--- calculadora_unidade_medida/main.py
+++ calculadora_unidade_medida/main.py
@@ -15,13 +15,17 @@ janela.resizable(False, False)
 janela.configure(bg = cor3)
 
 # Frame janela cima
-frame_cima = Frame(janela, width=550, height=75, bg='Red', pady=0, padx=3, relief=SOLID)
+frame_cima = Frame(janela, width=550, height=75, bg=cor2, pady=0, padx=3, relief=SOLID)
 frame_cima.place(x=5, y=5)
 # Janela esquerda
-frame_esquerda = Frame(janela, width=550, height=310, bg='Green', pady=0, padx=3, relief=SOLID)
+frame_esquerda = Frame(janela, width=550, height=310, bg=cor2, pady=0, padx=3, relief=SOLID)
 frame_esquerda.place(x=5, y=85)
 # Janela direita
-frame_direita = Frame(janela, width=232, height=390, bg='Blue', pady=0, padx=3, relief=SOLID)
+frame_direita = Frame(janela, width=232, height=390, bg=cor2, pady=0, padx=3, relief=SOLID)
 frame_direita.place(x=561, y=5)
+
+# Tema janela
+estilo = ttk.Style(janela)
+estilo.theme_use("clam")
 
 janela.mainloop()
