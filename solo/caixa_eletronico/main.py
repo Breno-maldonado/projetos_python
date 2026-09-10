98e1a39e09918b72c01712501d77c9ce20d58ecf
Breno Maldonado <maldonadobreno12@hotmail.com>
2026-09-09 21:48:14 -0300

    wip: deposito

--- solo/caixa_eletronico/main.py
+++ solo/caixa_eletronico/main.py
@@ -1,19 +1,35 @@
 saldo = 500.0
 
+def depositar(saldo_atual):
+    print("Digite o valor de deposito: ")
+    deposito = float(input("R$ "))
+    if deposito <= 0:
+        print("Valor invalido!")
+    else:
+        saldo_atual += deposito
+        print()
+        print(f'Valor de R$ {deposito} reais, depositado com sucesso!','\n')
+    return saldo_atual
+
 while True:
     print('Caixa Eletronico\n',
         '[1] Ver Saldo \n',
         '[2] Depositar \n',
         '[3] Sacar \n',
         '[4] Sair')
+    
+    try:
+        usuario = int(input('Digite o número da sua opção: '))
+    except ValueError:
+        print("Opção inválida! Por favor, digite apenas números inteiros.\n")
+        continue
 
-    usuario = int(input('Digite o número da sua opção: '))
     print()
 
     if usuario == 1:
         print(f'Seu saldo é de R$',saldo,'reais','\n')
     elif usuario == 2:
-        print("Em construção")
+       saldo = depositar(saldo)
     elif usuario == 3:
         print("Em construção")
     elif usuario == 4:
