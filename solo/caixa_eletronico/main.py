7a201d87b9ced5a6687b5226911de488661266aa
Breno Maldonado <maldonadobreno12@hotmail.com>
2026-09-10 20:12:47 -0300

    Caixa Eletronico

--- solo/caixa_eletronico/main.py
+++ solo/caixa_eletronico/main.py
@@ -1,14 +1,42 @@
 saldo = 500.0
 
 def depositar(saldo_atual):
+
     print("Digite o valor de deposito: ")
-    deposito = float(input("R$ "))
+
+    try:
+        deposito = float(input("R$ "))
+    except ValueError:
+        print()
+        print("Opção inválida! Por favor, digite apenas números!\n")
+        return saldo_atual
+    
     if deposito <= 0:
         print("Valor invalido!")
     else:
         saldo_atual += deposito
         print()
         print(f'Valor de R$ {deposito} reais, depositado com sucesso!','\n')
+    return saldo_atual
+
+def sacar(saldo_atual):
+
+    print("Qual o valor do saque?")
+
+    try:
+        saque = float(input("R$ "))
+    except ValueError:
+        print()
+        print("Opção inválida! Por favor, digite apenas números!\n")
+        return saldo_atual
+
+    if saque <= 0 or saque > saldo_atual:
+        print("\nValor inválido ou saldo insuficiente!")
+    else:
+        saldo_atual = saldo_atual - saque
+        print()
+        print(f"Valor de R$ {saque} reais, sacado com sucesso!", '\n')
+        
     return saldo_atual
 
 while True:
@@ -31,7 +59,7 @@ while True:
     elif usuario == 2:
        saldo = depositar(saldo)
     elif usuario == 3:
-        print("Em construção")
+        saldo = sacar(saldo)
     elif usuario == 4:
         break
     else:
